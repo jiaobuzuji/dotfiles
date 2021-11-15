58bd2b2e1e2b4b2af5784d3966098fc8330511d7
Jiaobuzuji <Jiaobuzuji@163.com>
2021-11-15 22:35:58 +0800

    nv

--- shell/basic.sh
+++ shell/basic.sh
@@ -36,6 +36,7 @@ alias gv='gvim'
 alias v='vim'
 alias vi='vim -u NONE' # pure vim
 alias gvi='gvim -u NONE'
+alias nv='nvim'
 
 alias ssh='ssh -Y' # Enables trusted X11 forwarding
 # alias sshn='ssh -YN' # Just forwarding ports
