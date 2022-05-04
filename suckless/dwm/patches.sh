a0790dfbaa050134d11716820ef946fb4107191e
Jiaobuzuji <Jiaobuzuji@163.com>
2022-05-04 14:25:20 +0800

    st patch

--- suckless/dwm/patches.sh
+++ suckless/dwm/patches.sh
@@ -6,20 +6,20 @@ cd patches
 # actualfullscreen fixborders noborderfloatingfix
 curl -OfSL https://dwm.suckless.org/patches/autostart/dwm-autostart-20210120-cb3f58a.diff
 curl -OfSL https://dwm.suckless.org/patches/systray/dwm-systray-6.3.diff
-curl -OfSL https://dwm.suckless.org/patches/xfce4-panel/dwm-xfce4-panel-20220306-d39e2f3.diff
+# curl -OfSL https://dwm.suckless.org/patches/xfce4-panel/dwm-xfce4-panel-20220306-d39e2f3.diff
+# curl -fSLO "https://dwm.suckless.org/patches/fullscreen/dwm-fullscreen-6.2.diff"
 curl -OfSL https://dwm.suckless.org/patches/actualfullscreen/dwm-actualfullscreen-20211013-cb3f58a.diff
 
 cd ..
 
-  # curl -fSLO "https://dwm.suckless.org/patches/fullscreen/dwm-fullscreen-6.2.diff"
-  # curl -fSLO "https://dwm.suckless.org/patches/actualfullscreen/dwm-actualfullscreen-20211013-cb3f58a.diff"
-  # curl -fSLO "https://dwm.suckless.org/patches/alpha/dwm-alpha-20201019-61bb8b2.diff"
-  # curl -fSLO "https://dwm.suckless.org/patches/alpha/dwm-fixborders-6.2.diff"
-  # curl -fSLO "https://dwm.suckless.org/patches/noborder/dwm-noborder-6.2.diff"
-  # curl -fSLO "https://dwm.suckless.org/patches/noborder/dwm-noborderfloatingfix-6.2.diff"
-  # curl -fSLO "https://dwm.suckless.org/patches/xresources/dwm-xresources-20210827-138b405.diff"
-  # curl -fSLO "https://dwm.suckless.org/patches/systray/dwm-systray-6.3.diff"
-  # curl -fSLO "https://dwm.suckless.org/patches/tatami/dwm-tatami-6.2.diff"
-  # curl -fSLO "https://dwm.suckless.org/patches/focusmaster/"
-  # curl -fSLO "https://dwm.suckless.org/patches/dwmc/dwm-dwmc-6.2.diff"
+# curl -fSLO "https://dwm.suckless.org/patches/actualfullscreen/dwm-actualfullscreen-20211013-cb3f58a.diff"
+# curl -fSLO "https://dwm.suckless.org/patches/alpha/dwm-alpha-20201019-61bb8b2.diff"
+# curl -fSLO "https://dwm.suckless.org/patches/alpha/dwm-fixborders-6.2.diff"
+# curl -fSLO "https://dwm.suckless.org/patches/noborder/dwm-noborder-6.2.diff"
+# curl -fSLO "https://dwm.suckless.org/patches/noborder/dwm-noborderfloatingfix-6.2.diff"
+# curl -fSLO "https://dwm.suckless.org/patches/xresources/dwm-xresources-20210827-138b405.diff"
+# curl -fSLO "https://dwm.suckless.org/patches/systray/dwm-systray-6.3.diff"
+# curl -fSLO "https://dwm.suckless.org/patches/tatami/dwm-tatami-6.2.diff"
+# curl -fSLO "https://dwm.suckless.org/patches/focusmaster/"
+# curl -fSLO "https://dwm.suckless.org/patches/dwmc/dwm-dwmc-6.2.diff"
 
--- suckless/st/patches.sh
+++ suckless/st/patches.sh
@@ -3,7 +3,9 @@ mkdir -p patches
 cd patches
 
 # patch : alpha
-# curl -OfSL https://dwm.suckless.org/patches/autostart/dwm-autostart-20210120-cb3f58a.diff
+curl -OfSL https://st.suckless.org/patches/scrollback/st-scrollback-20210507-4536f46.diff
+curl -OfSL https://st.suckless.org/patches/hidecursor/st-hidecursor-0.8.3.diff
+curl -OfSL https://st.suckless.org/patches/dracula/st-dracula-0.8.5.diff
 
 cd ..
 
