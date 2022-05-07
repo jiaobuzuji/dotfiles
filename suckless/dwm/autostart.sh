ea1668f79bb41437bd17f741b45d755ae5a231f3
Jiaobuzuji <Jiaobuzuji@163.com>
2022-05-07 23:40:27 +0800

    picom el8

--- suckless/dwm/autostart.sh
+++ suckless/dwm/autostart.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
+picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 slstatus & # dwmstatus &
 fcitx & # ibus &
 nm-applet &
 xfce4-power-manager &
+#xfce4-volumed-pulse &
