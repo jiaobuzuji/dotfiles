1bf3b7478e261be7c6c7400a75b1fbeffb6a8456
Jiaobuzuji <Jiaobuzuji@163.com>
2022-05-22 22:15:52 +0800

    patch move

--- suckless/dwm/patches.sh
+++ suckless/dwm/patches.sh
@@ -12,12 +12,11 @@ curl -fSLO "https://dwm.suckless.org/patches/focusadjacenttag/dwm-focusadjacentt
 curl -fSLO "https://dwm.suckless.org/patches/viewontag/dwm-viewontag-20210312-61bb8b2.diff"
 curl -fSLO "https://dwm.suckless.org/patches/movestack/dwm-movestack-20211115-a786211.diff"
 curl -fSLO "https://dwm.suckless.org/patches/movecenter/dwm-movecenter-6.2.diff"
-curl -fSLO "https://dwm.suckless.org/patches/rotatestack/dwm-rotatestack-20161021-ab9571b.diff"
 
 # curl -fSLO "https://dwm.suckless.org/patches/stackmfact/dwm-6.0-smfact.diff"
 # curl -fSLO "https://dwm.suckless.org/patches/alpha/dwm-alpha-20201019-61bb8b2.diff"
 # curl -fSLO "https://dwm.suckless.org/patches/alpha/dwm-fixborders-6.2.diff"
-curl -fSLO "https://dwm.suckless.org/patches/noborder/dwm-noborder-6.2.diff"
+# curl -fSLO "https://dwm.suckless.org/patches/noborder/dwm-noborder-6.2.diff"
 # curl -fSLO "https://dwm.suckless.org/patches/focusmaster/"
 # curl -fSLO "https://dwm.suckless.org/patches/focusonclick/dwm-focusonclick-20200110-61bb8b2.diff"
 # curl -fSLO "https://dwm.suckless.org/patches/fullscreen/dwm-fullscreen-6.2.diff"
@@ -26,6 +25,7 @@ curl -fSLO "https://dwm.suckless.org/patches/noborder/dwm-noborder-6.2.diff"
 cd ..
 
 # curl -fSLO "https://dwm.suckless.org/patches/awesomebar/dwm-awesomebar-20200907-6.2.diff"
+# curl -fSLO "https://dwm.suckless.org/patches/rotatestack/dwm-rotatestack-20161021-ab9571b.diff"
 # curl -fSLO "https://dwm.suckless.org/patches/gridmode/dwm-gridmode-20170909-ceac8c9.diff"
 # curl -OfSL https://dwm.suckless.org/patches/xfce4-panel/dwm-xfce4-panel-20220306-d39e2f3.diff
 # curl -fSLO "https://dwm.suckless.org/patches/noborder/dwm-noborderfloatingfix-6.2.diff"
